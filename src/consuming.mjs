25a3e63506628c6436e3592d439e8ea7a21ddd3a
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-06-04 11:32:43 +0530

    feat: add waiting-message with .finally()

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -44,4 +44,20 @@ export function chainCatch() {
 }
 
 export function final() {
+    showWaiting();
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+         return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+    .then(({data}) => {
+        setText(`City: ${data.city}`);
+    })
+    .catch(err => setText(err.message))
+    .finally(() => {
+        setTimeout(() => {
+            hideWaiting()
+        }, 1500);  
+
+        appendText("--Completey Done--");
+    }); 
 }
